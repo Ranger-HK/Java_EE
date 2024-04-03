e6afdd53ead9853026aea12e8760e4e43c4337e0
Ranger-HK <maxravindu99@gmail.com>
2024-04-03 20:00:07 +0530

    Updated

--- 01_Hello_Servlet/src/HelloServlet.java
+++ 01_Hello_Servlet/src/HelloServlet.java
@@ -1,8 +1,25 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 /**
  * @Created By Ravindu Prathibha
  * @created 4/3/2024 - 3:53 PM
  * @project Java_EE
  */
-public class HelloServlet {
 
+@WebServlet(urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Requests Awa");
+        PrintWriter writer = resp.getWriter();
+        writer.write("Response Sended");
+
+    }
 }
