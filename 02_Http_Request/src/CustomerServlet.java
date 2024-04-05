d2f3c82a12f1ab37d72b244128723ad6087aaf3c
Ranger-HK <maxravindu99@gmail.com>
2024-04-05 17:42:45 +0530

    Updated

---
+++ 02_Http_Request/src/CustomerServlet.java
@@ -0,0 +1,21 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @Created By Ravindu Prathibha
+ * @created 4/5/2024 - 4:36 PM
+ * @project Java_EE
+ */
+@WebServlet(urlPatterns = "/customer")
+public class CustomerServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.write("I am from customerServlet");
+    }
+}
