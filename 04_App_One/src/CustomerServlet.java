307e16d7fed8412be8601402c62228f65d618a1b
Ranger-HK <maxravindu99@gmail.com>
2024-04-09 21:08:44 +0530

    Updated

--- 04_App_One/src/CustomerServlet.java
+++ 04_App_One/src/CustomerServlet.java
@@ -4,6 +4,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  * @Created By Ravindu Prathibha
@@ -16,6 +21,37 @@ public class CustomerServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("App One");
+        //Create DB Connection
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Thogakade", "root", "19990202Ravi@:&pra");
+            ResultSet rst = connection.prepareStatement("SELECT  * FROM Customer").executeQuery();
+            String allRecords = "";
+
+            //Access the record and generate a json object
+            while (rst.next()) {
+                String customerId = rst.getString(1);
+                String name = rst.getString(2);
+                String address = rst.getString(3);
+                double salary = rst.getDouble(4);
+                System.out.println(customerId + " " + name + " " + address + " " + salary);
+
+                //Convert One Record To JSon
+                String customer = "{\"customerId\":\"" + customerId + "\",\"name\":\"" + name + "\",\"address\":\"" + address + "\",\"salary\":" + salary + "},";
+                allRecords = allRecords + customer;
+            }
+
+            //Array Create and fix ', ' problem
+            String finalJson = "[" + allRecords.substring(0, allRecords.length() - 1) + "]";
+
+            //print response
+            PrintWriter writer = resp.getWriter();
+            writer.write(finalJson);
+            //possible response types -->> text,xml,html,jason
+            /*resp.setContentType("application/json");*/
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
