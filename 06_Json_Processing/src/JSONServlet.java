9e37897b2e6ab2f768a0750488d25a1848c88750
Ranger-HK <maxravindu99@gmail.com>
2024-04-24 18:20:09 +0530

    Updated

--- 06_Json_Processing/src/JSONServlet.java
+++ 06_Json_Processing/src/JSONServlet.java
@@ -42,10 +42,10 @@ public class JSONServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        resp.setContentType("application/json");
+        resp.setContentType("application/json"); //introduce browser to json format using MIME tags
 
         // How to generate a single JSON object using JSON processing
-      /*  JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+      /*  JsonObjectBuilder objectBuilder = Json.createObjectBuilder(); //create json object
 
         objectBuilder.add("id","C001");
         objectBuilder.add("name","Tonny");
@@ -59,25 +59,25 @@ public class JSONServlet extends HttpServlet {
 
 
         // How to generate a  JSON Array using JSON processing
-        JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
+        JsonArrayBuilder arrayBuilder = Json.createArrayBuilder(); //create json array
 
-        JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+        JsonObjectBuilder objectBuilder = Json.createObjectBuilder(); //create json object one
         objectBuilder.add("id","C001");
         objectBuilder.add("name","Tonny");
         objectBuilder.add("address","Colombo");
         objectBuilder.add("salary",100000.00);
 
-        JsonObjectBuilder objectBuilder2 = Json.createObjectBuilder();
+        JsonObjectBuilder objectBuilder2 = Json.createObjectBuilder(); //create json object two
         objectBuilder2.add("id","C002");
         objectBuilder2.add("name","CJ");
         objectBuilder2.add("address","USA");
         objectBuilder2.add("salary",400000.00);
 
-        arrayBuilder.add(objectBuilder.build());
-        arrayBuilder.add(objectBuilder2.build());
+        arrayBuilder.add(objectBuilder.build()); //add json object one to the json array
+        arrayBuilder.add(objectBuilder2.build());//add json object two to the json array
 
         PrintWriter writer = resp.getWriter();
-        writer.print(arrayBuilder.build());
+        writer.print(arrayBuilder.build()); //print the json array as the response
 
 
     }
