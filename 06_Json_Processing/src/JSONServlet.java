ff5309006b13865d6d158f0675e6d08f9b533f26
Ranger-HK <maxravindu99@gmail.com>
2024-04-24 18:26:32 +0530

    Updated

--- 06_Json_Processing/src/JSONServlet.java
+++ 06_Json_Processing/src/JSONServlet.java
@@ -16,28 +16,6 @@ import java.io.PrintWriter;
 @WebServlet(urlPatterns = "/json")
 public class JSONServlet extends HttpServlet {
 
-    @Override
-    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-       /* System.out.println("Method Invoked");
-
-        ServletInputStream inputStream = req.getInputStream();
-
-        int read;
-        while ((read=inputStream.read())!=-1){
-            System.out.print((char) read);
-        }*/
-
-
-        System.out.println("Do Put Request Received");
-        JsonReader reader = Json.createReader(req.getReader());
-        JsonObject jsonObject = reader.readObject();
-        String customerID = jsonObject.getString("id");
-        String customerName = jsonObject.getString("name");
-        String customerAddress = jsonObject.getString("address");
-        String customerSalary = jsonObject.getString("salary");
-        System.out.println(customerID + " " + customerName + " " + customerAddress + " " + customerSalary);
-    }
-
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -62,16 +40,16 @@ public class JSONServlet extends HttpServlet {
         JsonArrayBuilder arrayBuilder = Json.createArrayBuilder(); //create json array
 
         JsonObjectBuilder objectBuilder = Json.createObjectBuilder(); //create json object one
-        objectBuilder.add("id","C001");
-        objectBuilder.add("name","Tonny");
-        objectBuilder.add("address","Colombo");
-        objectBuilder.add("salary",100000.00);
+        objectBuilder.add("id", "C001");
+        objectBuilder.add("name", "Tonny");
+        objectBuilder.add("address", "Colombo");
+        objectBuilder.add("salary", 100000.00);
 
         JsonObjectBuilder objectBuilder2 = Json.createObjectBuilder(); //create json object two
-        objectBuilder2.add("id","C002");
-        objectBuilder2.add("name","CJ");
-        objectBuilder2.add("address","USA");
-        objectBuilder2.add("salary",400000.00);
+        objectBuilder2.add("id", "C002");
+        objectBuilder2.add("name", "CJ");
+        objectBuilder2.add("address", "USA");
+        objectBuilder2.add("salary", 400000.00);
 
         arrayBuilder.add(objectBuilder.build()); //add json object one to the json array
         arrayBuilder.add(objectBuilder2.build());//add json object two to the json array
@@ -81,4 +59,35 @@ public class JSONServlet extends HttpServlet {
 
 
     }
+
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       /* System.out.println("Method Invoked");
+
+        ServletInputStream inputStream = req.getInputStream();
+
+        int read;
+        while ((read=inputStream.read())!=-1){
+            System.out.print((char) read);
+        }*/
+
+
+        System.out.println("Do Put Request Received");
+        JsonReader reader = Json.createReader(req.getReader());
+        JsonObject jsonObject = reader.readObject();
+        String customerID = jsonObject.getString("id");
+        String customerName = jsonObject.getString("name");
+        String customerAddress = jsonObject.getString("address");
+        String customerSalary = jsonObject.getString("salary");
+        System.out.println(customerID + " " + customerName + " " + customerAddress + " " + customerSalary);
+    }
+
+
 }
