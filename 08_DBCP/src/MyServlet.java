8184edafe2cb0f2c8443b0a5d154acf2ea531867
Ranger-HK <maxravindu99@gmail.com>
2024-04-30 14:10:22 +0530

    Updated

---
+++ 08_DBCP/src/MyServlet.java
@@ -0,0 +1,34 @@
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/hello")
+public class MyServlet extends HttpServlet {
+
+    //Life Cycle Of Servlet
+    public MyServlet() {
+        System.out.println("Object is created");
+    }
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+
+        System.out.println("I'm servlet now");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        System.out.println("Get Request is Generated");
+    }
+
+    @Override
+    public void destroy() {
+
+        System.out.println("The end");
+    }
+}
