b837730bfbf98dbb59f35533c9f021b21875fe6e
Ranger-HK <maxravindu99@gmail.com>
2024-05-01 19:22:24 +0530

    Updated

---
+++ 09_Filters/src/filters/MyFilter.java
@@ -0,0 +1,42 @@
+package filters;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/*@WebFilter(urlPatterns = {"/customer","/item","/order"})*/ //---> selected url mapping
+//@WebFilter(urlPatterns = "/*") //---> multi url mapping
+@WebFilter(urlPatterns = "/item") //---> single url mapping
+public class MyFilter implements Filter {
+
+    public MyFilter() {
+        /*System.out.println("Object Created from MyFilter");*/
+        System.out.println("Object Was Created From My Filter Class");
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        /*System.out.println("My Filter Initialized");*/
+        System.out.println("Now MyFilter Class is a processing Filter");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        /*System.out.println("Do Filter Method called");*/
+        System.out.println("First");
+
+        filterChain.doFilter(servletRequest, servletResponse);
+
+        PrintWriter writer = servletResponse.getWriter();
+        writer.write("Added from MyFilter");
+
+        System.out.println("Second");
+    }
+
+    @Override
+    public void destroy() {
+        /*System.out.println("My Filter Destroyed");*/
+        System.out.println("My Filter Was Destroyed");
+    }
+}
---
+++ 09_Filters/src/servlet/OrderServlet.java
@@ -0,0 +1,19 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/order")
+public class OrderServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.write("Purchase Order Do Get Method Invoked");
+        System.out.println("Purchase Order Do Get Method Invoked");
+    }
+}
